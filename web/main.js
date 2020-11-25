d07ec26c831b9ee613e7db8669c69835eaf2cdb4
steven.liatti <steven@liatti.ch>
2020-11-25 15:10:41 +0100

    No need to custom HSL function, cytoscape builtin exists

--- web/main.js
+++ web/main.js
@@ -16,36 +16,6 @@ function popupManagement(cy, popupFn, ms) {
   })
 }
 
-// Convert HSL color to hex value, thanks to https://stackoverflow.com/a/44134328
-function hslToHex(h, s, l) {
-  h /= 360;
-  s /= 100;
-  l /= 100;
-  let r, g, b;
-  if (s === 0) {
-    r = g = b = l; // achromatic
-  } else {
-    const hue2rgb = (p, q, t) => {
-      if (t < 0) t += 1;
-      if (t > 1) t -= 1;
-      if (t < 1 / 6) return p + (q - p) * 6 * t;
-      if (t < 1 / 2) return q;
-      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-      return p;
-    };
-    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-    const p = 2 * l - q;
-    r = hue2rgb(p, q, h + 1 / 3);
-    g = hue2rgb(p, q, h);
-    b = hue2rgb(p, q, h - 1 / 3);
-  }
-  const toHex = x => {
-    const hex = Math.round(x * 255).toString(16);
-    return hex.length === 1 ? '0' + hex : hex;
-  };
-  return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
-}
-
 Promise.all([
   fetch('cy-style.json').then(res => res.json()),
   getActors(5, 5, 5)
@@ -123,7 +93,7 @@ Promise.all([
 
   cy.style()
     .selector('node')
-    .style('background-color', (ele) => hslToHex(ele.data('knowsCommunity') % 360, 100, 50))
+    .style('background-color', (ele) => `hsl(${ele.data('knowsCommunity') % 360}, 100%, 50%)`)
     .update()
 
   console.log(graph)
