774aca94dc5bace7d4835ab9adad921fbea01f3c
jeremy.favre <jeremy.favre@etu.hesge.ch>
2020-12-08 23:53:30 +0100

    Tuto done

---
+++ web/scripts/tuto.js
@@ -0,0 +1,34 @@
+let tutoImg = ["./img/img1.png", "./img/img2.jpg", "./img/img3.png"]
+
+let currentIdx = 0;
+let btnBack = document.getElementById("btnBack");
+let btnNext = document.getElementById("btnNext");
+
+btnBack.style.visibility = 'hidden';
+
+
+function back() {
+    currentIdx--;
+    $('#tutoImg').attr("src", tutoImg[currentIdx]);
+    if(currentIdx === 0)
+        btnBack.style.visibility = 'hidden';
+    if(btnNext.style.visibility === 'hidden')
+        btnNext.style.visibility = 'visible';
+}
+
+function next() {
+    if(btnBack.style.visibility === 'hidden')
+        btnBack.style.visibility = 'visible';
+    currentIdx++;
+    $('#tutoImg').attr("src", tutoImg[currentIdx]);
+    if(currentIdx === tutoImg.length-1)
+        btnNext.style.visibility = 'hidden';
+
+}
+
+function resetTuto() {
+    currentIdx = 0;
+    $('#tutoImg').attr("src", tutoImg[currentIdx]);
+    btnBack.style.visibility = 'hidden';
+    btnNext.style.visibility = 'visible';
+}
